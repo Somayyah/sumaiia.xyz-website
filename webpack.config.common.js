991dfc9fcf271b30400513d8ca413d76766a6278
somayyah <smalfraihat131@cit.just.edu.jo>
2021-02-10 20:44:48 +0200

    minified

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: '[name].[contenthash].js', // this line is the only difference
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
