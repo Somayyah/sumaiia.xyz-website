4f85ec732a3ac7a25be5ca00fe6017b1f3a31bb9
somayyah <smalfraihat131@cit.just.edu.jo>
2021-02-13 22:54:34 +0200

    animations added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import Head from 'next/head'
 import { GetStaticProps } from 'next'
 import parse from 'html-react-parser'
-import { motion } from "framer-motion"
+import { motion, useViewportScroll } from "framer-motion"
+
 
 export default function Home(props) {
+  const { scrollYProgress } = useViewportScroll()
+
   return (
-    <>
+    <div >
       <Head>
         <meta charSet="utf-8" />
         <meta
@@ -13,19 +16,17 @@ export default function Home(props) {
           content={props.content} />
         <title>{props.title}</title>
       </Head>
-      <motion.div className="introduction" animate={{ opacity: 1 }} initial={{ opacity: 0 }} transition={{ duration: 1 }}>
+      <motion.div className="introduction" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.8 }}>
         {parse(props.introduction.content)}
       </motion.div>
-      <motion.div className='WhatIUse' animate={{ opacity: 1 }} initial={{ opacity: 0 }} transition={{ duration: 1 }}>
-        <h1>What I use</h1>
+      <div className='WhatIUse'>
+        <motion.h1 initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>What I use</motion.h1>
         <hr />
-        <ul>
-          {props.WhatIUse.list.map((item, index) => (
-            <li key={`WhatIUse${index}`}>{item}</li>
-          ))}
-        </ul>
-      </motion.div>
-      <motion.div className="ContactInfo" animate={{ opacity: 1 }} initial={{ opacity: 0 }} transition={{ duration: 1 }}>
+        {props.WhatIUse.list.map((item, index) => (
+          <motion.img key={`WhatIUse${index}`} src={item}  initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}/>
+        ))}
+      </div>
+      <motion.div className="ContactInfo" style={{ scale: scrollYProgress }}>
         <h1>Find me at</h1>
         <hr />
         <ul>
@@ -34,7 +35,7 @@ export default function Home(props) {
           ))}
         </ul>
       </motion.div>
-    </>
+    </div>
   )
 }
 
@@ -51,13 +52,20 @@ export const getStaticProps: GetStaticProps = async () => {
 
   const WhatIUse: template = {
     title: "What I use",
-    list: ["next js",
-      "React js",
-      "C++",
-      "Linux",
-      "Docker",
-      "Google Cloud Platform",
-      "HTML5/CSS3/JS/TS"]
+    list: [
+      "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png",
+      "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png",
+      "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/sass/sass.png",
+      "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png",
+      "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png",
+      "https://iconape.com/wp-content/files/an/351546/svg/tailwind-css-seeklogo.com.svg",
+      "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png",
+      "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongodb/mongodb.png",
+      "https://www.docker.com/sites/default/files/d8/2019-07/Moby-logo.png",
+      "https://upload.wikimedia.org/wikipedia/commons/e/e9/Jenkins_logo.svg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Linux_Logo_in_Linux_Libertine_Font.svg/1200px-Linux_Logo_in_Linux_Libertine_Font.svg.png",
+      "https://newhorizons.com.sg/wp-content/uploads/2019/04/python.png"
+    ]
   }
 
   const introduction: intro = {
