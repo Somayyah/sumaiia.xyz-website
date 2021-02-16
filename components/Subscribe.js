baaf471b851f4410b1a615b98a12415075f38714
somayyah <somayyahmohd95@gmail.com>
2021-02-16 10:04:52 +0000

    mailchimp added

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -1,58 +1,49 @@
-import { useRef, useState } from 'preact/hooks';
+import { useState } from "preact/hooks"
+import axios from "axios"
+import styles from "../styles/newsletter.module.css"
 
-function Subscribe() {
-  // 1. Create a reference to the input so we can fetch/clear it's value.
-  const inputEl = useRef(null);
-  // 2. Hold a message in state to handle the response from our API.
-  const [message, setMessage] = useState('');
+export default () => {
+  const [email, setEmail] = useState("")
+  const [state, setState] = useState("IDLE")
+  const [errorMessage, setErrorMessage] = useState(null)
 
-  const subscribe = async (e) => {
-    e.preventDefault();
-
-    // 3. Send a request to our API with the user's email address.
-    const res = await fetch('/api/subscribe', {
-      body: JSON.stringify({
-        email: inputEl.current.value
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: 'POST'
-    });
-
-    const { error } = await res.json();
-
-    if (error) {
-      // 4. If there was an error, update the message in state.
-      setMessage(error);
-
-      return;
+  const subscribe = async () => {
+    setState("LOADING")
+    setErrorMessage(null)
+    try {
+      const response = await axios.post("/api/newsletter", { email })
+      setState("SUCCESS")
+    } catch (e) {
+      setErrorMessage(e.response.data.error)
+      setState("ERROR")
     }
-
-    // 5. Clear the input value and show a success message.
-    inputEl.current.value = '';
-    setMessage('Success! 🎉 You are now subscribed to the newsletter.');
-  };
+  }
 
   return (
-    <form onSubmit={() => subscribe}>
-      <label htmlFor="email-input">{'Email Address'}</label>
-      <input
-        id="email-input"
-        name="email"
-        placeholder="you@awesome.com"
-        ref={inputEl}
-        required
-        type="email"
-      />
+    <div className={styles.container}>
+      <h2 className={styles.header}>Subscribe to my newsletter!</h2>
+      <p className={styles.details}>
+        It includes interesting tech content as well as some information on how
+        to live life to the fullest!
+      </p>
       <div>
-        {message
-          ? message
-          : `I'll only send emails when new content is posted. No spam.`}
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button
+          type="button"
+          disabled={state === "LOADING"}
+          onClick={subscribe}
+        >
+          {state === "LOADING" ? "Loading" : "Subscribe"}
+        </button>
       </div>
-      <button type="submit">{'✨ Subscribe 💌'}</button>
-    </form>
-  );
+      {state === "ERROR" && <p className={styles.errorMsg}>{errorMessage}</p>}
+      {state === "SUCCESS" && <p className={styles.successMsg}>Success!</p>}
+    </div>
+  )
 }
-
-export default subscribe
